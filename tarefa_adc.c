02f23848cabad58f81fd4a961c3ba05fd56d28c3
Andre <andrelimatecnologias@gmail.com>
2025-02-11 23:00:34 -0300

    Configuração e inicialização da comunicação com o display OLED

--- tarefa_adc.c
+++ tarefa_adc.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "bibliotecas/ssd1306.h"
+#include "bibliotecas/font.h"
 
 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define ENDERECO 0x3C
 
+#define INTENSIDADE 0.01
+
+ssd1306_t ssd; // Inicializa a estrutura do display
+
+void configuracao(){
+
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, ENDERECO, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+}
 
 int main()
 {
     stdio_init_all();
+
+    configuracao();
 
     while (true) {
         printf("Hello, world!\n");
