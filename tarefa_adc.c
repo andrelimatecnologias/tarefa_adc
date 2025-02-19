668c98b6292afe53a3435a123099064fd5788777
Andre <andrelimatecnologias@gmail.com>
2025-02-18 22:37:24 -0300

    Explicação de cada seção do código adicionada, e finalização do projeto

--- tarefa_adc.c
+++ tarefa_adc.c
@@ -10,37 +10,50 @@
 #include <math.h>
 
 
+// As variáveis a seguir definem qual a porta que será utilizada para comunicação i2c com o display, os pinos utilizados na comunicação e o endereço do display
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define ENDERECO 0x3C
 
-#define INTENSIDADE 0.01
-
+// definição dos pinos para acionamento do LED RGB
 #define GPIO_LED_R 13
 #define GPIO_LED_G 11
 #define GPIO_LED_B 12
 
+// definição dos pinos dos botões utilizados no projeto
 #define BUTTON_A 5
 #define BUTTON_B 6
 #define BUTTON_JOYSTIC 22
 
+// definição dos pinos utilizados no joystick
 #define JOYSTICK_Y 26
 #define JOYSTICK_X 27
 
+// definição da variável utilizada na configuração do sinal PWM para frequência, e para definição do duty cycle posteriormente
 #define PWM_WRAP 2048
+
+// definição da variável utilizada para garantir o desligamento dos LEDs quando o joystick está em repouso ao centro x,y.
 #define TOLERANCIA 15
 
+// definição das variáveis que lê a posição inicial do joystick a fim de utilizar uma referência segura do centro (x,y) para que o LED seja acionado no momento certo
 int16_t ajuste_fino_x = 0;
 int16_t ajuste_fino_y = 0;
 
+// definição da variável de controle que definirá se a animação dos LEDs está ativa, ou se somente a animação do display será produzida
 volatile bool LEDs_PWM_ativados = true;
+
+// definição da variável de controle da animação de uma borda mais fina ou mais grossa quando o acionamento do botão A acontece
 volatile bool borda_dupla=false;
+
+// definição das variáveis que calcularão em uma escala de 0 a 100% dos eixos x e y do display onde o quadrado será desenhado 
 float xp=0.0,yp=0.0;
 
-ssd1306_t ssd; // Inicializa a estrutura do display
-volatile uint32_t last_time=0;
 
+ssd1306_t ssd; // Inicializa a estrutura do display
+volatile uint32_t last_time=0;// variável utilizada para tratamento do debounce
+
+// Função utilizada para produção de uma borda mais fina ou mais grossa no display
 void pinta_borda_display(){
     ssd1306_rect(&ssd,3,3,WIDTH-5,HEIGHT-5,true,false);
     if(borda_dupla){
@@ -50,6 +63,7 @@ void pinta_borda_display(){
     ssd1306_send_data(&ssd);
 }
 
+// Função que atende individualmente as interrupções de cada um dos botões: produzindo a ativação/desativação da animação dos LEDs, a ativação do modo bootsel por código (facilita a adaptação rápida para testar o código) e o controle de borda e do LED verde
 void gpio_irq_handler(uint gpio, uint32_t events){
     uint32_t atual = to_ms_since_boot(get_absolute_time());
     if((atual-last_time)>200){
@@ -68,48 +82,60 @@ void gpio_irq_handler(uint gpio, uint32_t events){
     }
 }
 
+// Função que inicializa o programa, será executada uma vez no início
 void configuracao(){
+    
 
+    // inicialização das portas
     gpio_init(GPIO_LED_G);
     gpio_init(BUTTON_A);
     gpio_init(BUTTON_B);
     gpio_init(BUTTON_JOYSTIC);
 
+    // definição se são entradas ou saídas
     gpio_set_dir(GPIO_LED_G,GPIO_OUT);
     gpio_set_dir(BUTTON_A,GPIO_IN);
     gpio_set_dir(BUTTON_B, GPIO_IN);
     gpio_set_dir(BUTTON_JOYSTIC,GPIO_IN);
 
+    // acionamento dos resistores pull up para os botões
     gpio_pull_up(BUTTON_A);
     gpio_pull_up(BUTTON_B);
     gpio_pull_up(BUTTON_JOYSTIC);
 
+    // definição das interrupções para cada um dos botões
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BUTTON_JOYSTIC,GPIO_IRQ_EDGE_FALL,true,&gpio_irq_handler);
 
+    // desliga se o LED verde
     gpio_put(GPIO_LED_G,false);
 
 
+    // inicialização da comunicação I2C
     i2c_init(I2C_PORT, 400 * 1000);
 
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-    gpio_pull_up(I2C_SDA); // Pull up the data line
-    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    // escolha do pino utilizado na comunicação definindo o propósito que será destinada
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); 
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
+    gpio_pull_up(I2C_SDA); // O protocolo I2C para funcionar corretamente demanda um resistor de pull up
+    gpio_pull_up(I2C_SCL); // O protocolo I2C para funcionar corretamente demanda um resistor de pull up
     
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, ENDERECO, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd); // Configura o display
-    ssd1306_send_data(&ssd); // Envia os dados para o display
+    // configuração e inicialização do display com auxílio da biblioteca
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, ENDERECO, I2C_PORT); 
+    ssd1306_config(&ssd); 
+    ssd1306_send_data(&ssd);
 
     // Limpa o display. O display inicia com todos os pixels apagados.
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
+    // inicialização do ADC para leitura do joystick
     adc_init();
     adc_gpio_init(JOYSTICK_X);
     adc_gpio_init(JOYSTICK_Y);
 
+    // configuração e geração de dois sinais PWM para acionamento gradual dos LEDs vermelho e azul
     gpio_set_function(GPIO_LED_R, GPIO_FUNC_PWM);
     gpio_set_function(GPIO_LED_B,GPIO_FUNC_PWM);
     uint slice1 = pwm_gpio_to_slice_num(GPIO_LED_R);
@@ -122,6 +148,7 @@ void configuracao(){
     pwm_set_enabled(slice2,true);
 }
 
+// Função que calcula a intensidade a ser aplicada para gerar um duty cycle variando do centro para qualquer direção escolhida
 uint16_t getIntensidadeLED(int16_t medida){
     
     if(medida>2048){
@@ -130,6 +157,7 @@ uint16_t getIntensidadeLED(int16_t medida){
     return 2048-medida;
 }
 
+// Função que pinta o quadrado do tamanho de 8x8 pixels que será movimentado a partir das leituras realizadas no joystick produzindo uma animação
 void pintaQuadradoDisplay(float x, float y){
     ssd1306_fill(&ssd,false);
     pinta_borda_display();
@@ -145,9 +173,12 @@ void pintaQuadradoDisplay(float x, float y){
 
 int main()
 {
+    
     stdio_init_all();
 
     configuracao();
+
+    // leitura inicial para garantir a centralização dos valores x,y medidos desligando os LEDs ao centro do joystick
     adc_select_input(1);
     ajuste_fino_x = 2048-adc_read();
     adc_select_input(0);
@@ -156,16 +187,22 @@ int main()
     pwm_set_gpio_level(GPIO_LED_B,0);
 
     while (true) {
+        // leitura das posições x e y
         adc_select_input(1);
-        
         uint16_t leituraX = adc_read();
         xp = ((float)leituraX)/4095.0; 
         adc_select_input(0);
         uint16_t leituraY = adc_read();
         yp = ((float)leituraY)/4095.0;
+
+        // pinta no display o quadrado na devida posição x,y medida
         pintaQuadradoDisplay(xp,yp);
+
+        // considera o deslocamento padrão inicial produzido pelo joystick
         uint16_t corrigidoX = getIntensidadeLED(leituraX+ajuste_fino_x);
         uint16_t corrigidoY = getIntensidadeLED(leituraY+ajuste_fino_y);
+        
+        //considera um valor de tolerância devido instabilidade no eixo central do joystick, a fim de garantir que o LED seja apagado quando o josytick retorna ao centro.
         if((corrigidoX>TOLERANCIA)&&(corrigidoY>TOLERANCIA)&&(LEDs_PWM_ativados)){
             pwm_set_gpio_level(GPIO_LED_R,corrigidoX);
             pwm_set_gpio_level(GPIO_LED_B,corrigidoY);
